7b1ddda836350fec574438164e8d0146a36a77a4
stephen miracle <109660118+stephenmiracle4@users.noreply.github.com>
2024-06-09 21:44:59 +0100

    commit message
    commit message

--- whether-app/src/App.js
+++ whether-app/src/App.js
@@ -1,23 +1,37 @@
 
 import './App.css';
 import React, { useEffect, useState } from "react";
+import weather from './components/weather';
 export default function App() {
 
   const [lat, setLat] = useState([]);
   const [long, setLlong] = useState([]);
+  const [data, setDate] = useState([]);
   
   useEffect(() => {
+  const fetchData = async () => {
     navigator.geolocation.getCurrentPosition(function(position) {
       setLat(position.coords.latitude);
       setLlong(position.coords.longitude);
     });
-  
-    console.log("Latitude is:", lat)
-    console.log("Longitude is:", long)
+
+    await fetch( '${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}$units=metric$APPID=${process.env.REACT_APP_API_KEY}')
+    .then(res => res.json())
+    .then(result => {
+      setDate(result)
+        console.log(result);
+    });
+  }
+    fetchData();
   }, [lat, long]);
 
   return (
     <div className="App">
+      {(typeof data.main != 'undefined') ? (
+        <weather weatherData={data}/>
+      ): (
+       <div></div>
+      )}
 
     </div>
   );
